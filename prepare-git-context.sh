a7f2fd2f224912f8ba423a3b0dbf7549a306afc6
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-19 11:48:30 +0100

    gather last commit user details to be used when pushing

--- prepare-git-context.sh
+++ prepare-git-context.sh
@@ -7,9 +7,11 @@ git config remote.origin.url "git@github.com:${ORG_NAME}/${REPO_NAME}.git"
 # Enable to fetch branches when cloning with a detached and shallow clone
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 
-# Force the git user details when pushing
-git config --global user.email 'VictorMartinezRubio@gmail.com'
-git config --global user.name 'Victor Martinez'
+# Force the git user details when pushing using the last commit details
+USER_MAIL=$(git log -1 --pretty=format:'%ae')
+USER_NAME=$(git log -1 --pretty=format:'%an')
+git config --global user.email "${USER_MAIL}"
+git config --global user.name "${USER_NAME}"
 
 # Checkout the branch as it's detached based by default.
 # See https://issues.jenkins-ci.org/browse/JENKINS-33171
